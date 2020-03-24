f8ab97cba03468d983b42132ec99cae028caaf77
marc-despland <marc.despland@art122-5.net>
2020-03-24 09:40:40 +0100

    solve issues for creation pep-proxy

--- pep-proxy/nginx/docker-entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-envsubst < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
-rm -f /etc/nginx/conf.d/default.conf.template
-
-exec nginx -g 'daemon off;'
---
+++ pep-proxy/nginx/docker_entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+envsubst envsubst '$$NGINX_PORT $$NGINX_HOST $$PEP_PROXY_IP $$NGSI_AGENTS_IP $$ORION_IP' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+rm -f /etc/nginx/conf.d/default.conf.template
+
+exec nginx -g 'daemon off;'
