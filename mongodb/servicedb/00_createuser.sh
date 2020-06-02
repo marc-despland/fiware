63d3a66a0b6f69e28a544be447997b64a2b83bc4
marc-despland <marc.despland@art122-5.net>
2020-06-02 07:43:49 +0200

    mongodb

---
+++ mongodb/servicedb/00_createuser.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export MONGO_INITDB_ROOT_PASSWORD=`cat $MONGO_INITDB_ROOT_PASSWORD_FILE`
+export SERVICEDB_PASSWORD=`cat $SERVICEDB_PASSWORD_FILE`
+mongo -u $MONGO_INITDB_ROOT_USERNAME -p $MONGO_INITDB_ROOT_PASSWORD $MONGO_INITDB_DATABASE << EOF
+use "$SERVICEDB_DATABASE";
+db.createUser({ user: 'servicedb', pwd: "$SERVICEDB_PASSWORD", roles: [ { role: 'userAdmin', db: "$SERVICEDB_DATABASE"}, { role: 'readWrite', db: "$SERVICEDB_DATABASE"}, {role: 'dbAdmin', db: "$SERVICEDB_DATABASE"} ] } );
+EOF
